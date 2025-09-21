24dbece8296f8140082ed093d1b351658b648968
Krelle156 <kotofteng@gmail.com>
2025-09-21 14:43:58 +0200

    Implemented the StoreBælt system. It handles creating the vehicle, so the user of the library doesn't have to know the other libray.
    However using a string to check the type feels to me like a weak link. The rework required in the next task might be an opportunity to fix that.
    
    Tests failed initially because I first forgot that today was a weekend day and I lazily used DateTime.Now and then because I forgot to implement the discount being only for cars while checking the MC-price (Which I only did to bring up code coverage).
    
    Code coverage is alright when running for all tests. The StoreBælt tests does not check for things already checked in the other library I hope that is alright.
    
    All tests succeeded.

---
+++ StoreBaeltTicketLibrary/StoreBæltTicket.cs
@@ -0,0 +1,49 @@
+﻿using Bridge;
+
+namespace StoreBaeltTicketLibrary
+{
+    public class StoreBæltTicket
+    {
+        /// <summary>
+        /// Details of the vehicle and its standard price
+        /// </summary>
+        public Vehicle Vehicle { get; set; }
+
+        /// <summary>
+        /// The constuctor for creating a StoreBælt ticket.
+        /// </summary>
+        /// <param name="lisencePlate">The lisence plate for the vehicle</param>
+        /// <param name="type">The type of the vehicle expressed as a string...</param>
+        /// <exception cref="ArgumentException">The exception thrown if the type is not a car or a mc (motorcycle)</exception>
+        public StoreBæltTicket(string lisencePlate, DateTime date, string type)
+        {
+            if(type.ToLower() == "car")
+            {
+                Vehicle = new Bridge.Car(lisencePlate, date);
+            }
+            else if(type.ToLower() == "mc")
+            {
+                Vehicle = new Bridge.MC(lisencePlate, date);
+            }
+            else
+            {
+                throw new ArgumentException("Type must be either 'Car' or 'MC'");
+            }
+        }
+
+        /// <summary>
+        /// A function for getting the price of the ticket specific to StoreBælt.
+        /// </summary>
+        /// <returns>The price of the ticket</returns>
+        public double Price()
+        {
+            double basePrice = Vehicle.Price();
+
+            if (Vehicle.VehicleType() == "Car" && (Vehicle.Date.DayOfWeek == DayOfWeek.Saturday || Vehicle.Date.DayOfWeek == DayOfWeek.Sunday))
+            {
+                basePrice *= 0.85;
+            }
+            return basePrice;
+        }
+    }
+}
---
+++ StoreBaeltTicketLibraryTests/StoreBæltTicketTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoreBaeltTicketLibrary;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreBaeltTicketLibrary.Tests
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass()]
+    public class StoreBæltTicketTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InvalidTypeTest()
+        {
+            new StoreBæltTicket("AB12345", DateTime.Now, "Jason Bourne");
+        }
+
+        [TestMethod()]
+        public void PriceTestNotWeekend()
+        {
+            StoreBæltTicket testTicket = new StoreBæltTicket("AB12345", new DateTime(2025-09-22), "Car");
+            Assert.AreEqual(230, testTicket.Price());
+        }
+
+        [TestMethod()]
+        public void PriceTestWeekendCar()
+        {
+            StoreBæltTicket testTicket = new StoreBæltTicket("AB12345", new DateTime(2025, 09, 21), "Car");
+            Assert.AreEqual(230 * 0.85, testTicket.Price());
+        }
+
+        [TestMethod()]
+        public void PriceTestWeekendMC()
+        {
+            StoreBæltTicket testTicket = new StoreBæltTicket("AB12345", new DateTime(2025, 09, 21), "MC");
+            Assert.AreEqual(120, testTicket.Price());
+        }
+    }
+}
