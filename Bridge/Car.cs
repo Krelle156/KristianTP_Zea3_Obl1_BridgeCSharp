f71eda2434d4e41121cdd82837c92e22db9e5e77
Krelle156 <kotofteng@gmail.com>
2025-09-21 10:55:32 +0200

    Followed Task 2. Car defined with license plate and date(Of toll passage?) as well as functions for type and price (of passing toll). (Unnecessary) Tests implemented and successfully run.

---
+++ Bridge/Car.cs
@@ -0,0 +1,45 @@
+﻿namespace Bridge
+{
+    public class Car
+    {
+        /// <summary>
+        /// The licence plate of the car. No restrictions on getting and setting.
+        /// </summary>
+        public string LicencePlate { get; set; }
+
+        /// <summary>
+        /// Possibly the date where the car passes tollbooth (Not specified in assignment 1). No restrictions on getting and setting.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        //Opgave 1 nævner ikke constructor, men jeg tænker at det er godt at have på forhånd.
+        /// <summary>
+        /// Constructor for creating a car instance.
+        /// </summary>
+        /// <param name="licencePlate">The license plate of the car.</param>
+        /// <param name="date">TBD possibly the date of this car passing toll</param>
+        public Car(string licencePlate, DateTime date)
+        {
+            LicencePlate = licencePlate;
+            Date = date;
+        }
+
+        /// <summary>
+        /// A function that gets you the toll levied on this type of vehicle.
+        /// </summary>
+        /// <returns>A double representing the toll for this type of vehicle.</returns>
+        public double Price()
+        {
+            return 230;
+        }
+
+        /// <summary>
+        /// A function that returns a string representing the type of vehicle.
+        /// </summary>
+        /// <returns>A string containing the name of the vehicle's type</returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
---
+++ BridgeTests/CarTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bridge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Bridge.Tests
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass()]
+    public class CarTests
+    {
+        // Det her føles som overkill... er det virkelig værd at "teste"?
+
+        [TestMethod()]
+        public void CarTest()
+        {
+            DateTime testDate = new DateTime(2015, 11, 11);
+            Car testCar = new Car("AHAB1851", testDate);
+            Assert.AreEqual("AHAB1851", testCar.LicencePlate);
+            Assert.AreEqual(testDate, testCar.Date);
+        }
+
+        [TestMethod()]
+        public void PriceTest()
+        {
+            Car testCar = new Car("AHAB1851", DateTime.Now);
+            //Antager at prisen forbliver 230, per opgavebeskrivelsen, selvom det virker lidt udynamisk.
+            Assert.AreEqual(230, testCar.Price());
+        }
+
+        [TestMethod()]
+        public void VehicleTypeTest()
+        {
+            Car testCar = new Car("AHAB1851", DateTime.Now);
+            Assert.AreEqual("Car", testCar.VehicleType());
+        }
+    }
+}
